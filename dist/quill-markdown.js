395b3918594748b99178bbdaa21082935ecedb2a
Ryan Lester <hacker@linux.com>
2021-05-13 13:50:15 -0400

    update dependencies

--- dist/quill-markdown.js
+++ dist/quill-markdown.js
@@ -1,23 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.quillToMarkdown = exports.markdownToQuill = exports.markdownToHTML = exports.htmlToMarkdown = void 0;
 var MarkdownIt = require("markdown-it");
 var node_quill_converter_1 = require("node-quill-converter");
 var toMarkdown = require("to-markdown");
 var markdownIt = MarkdownIt();
 /** Converts an HTML string into a Markdown string. */
-exports.htmlToMarkdown = function (html) {
+var htmlToMarkdown = function (html) {
     return toMarkdown(html);
 };
+exports.htmlToMarkdown = htmlToMarkdown;
 /** Converts a Markdown string into a Quill delta. */
-exports.markdownToHTML = function (markdown) {
+var markdownToHTML = function (markdown) {
     return markdownIt.render(markdown);
 };
+exports.markdownToHTML = markdownToHTML;
 /** Converts a Markdown string into a Quill delta. */
-exports.markdownToQuill = function (markdown) {
+var markdownToQuill = function (markdown) {
     return node_quill_converter_1.convertHtmlToDelta(exports.markdownToHTML(markdown));
 };
+exports.markdownToQuill = markdownToQuill;
 /** Converts a Quill delta into a Markdown string. */
-exports.quillToMarkdown = function (delta) {
+var quillToMarkdown = function (delta) {
     return exports.htmlToMarkdown(node_quill_converter_1.convertDeltaToHtml(delta));
 };
+exports.quillToMarkdown = quillToMarkdown;
 //# sourceMappingURL=quill-markdown.js.map
